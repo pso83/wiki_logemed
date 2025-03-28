00f6c976f4df459a5bb37323d4792dc88bb56561
Paul Soulat <paul.soulat@logemed.fr>
2025-03-28 10:28:26 +0100

    Test_docker_5

--- Wiki.py
+++ Wiki.py
@@ -37,9 +37,9 @@ def allowed_file(filename):
 app.secret_key = 'your_secret_key'  # Nécessaire pour gérer les sessions
 
 # Configuration de la base de données pour logemed 3
-DB_HOST = '192.168.110.14'      # '194.164.60.164
-DB_USER = 'asoulat'             # 'devlog'
-DB_PASSWORD = '123@log'         # '123456Sou_log
+DB_HOST = '194.164.60.164'      # '194.164.60.164
+DB_USER = 'devlog'             # 'devlog'
+DB_PASSWORD = '123456Sou_log'         # '123456Sou_log
 DB_NAME = 'wiki_db'
 
 # Configuration FTP
